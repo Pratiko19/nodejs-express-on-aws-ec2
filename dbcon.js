315ef879a2ac918451c32316ca45c68f451ffef3
Pratik <kumar.pratik3@outlook.com>
2021-12-26 10:38:50 +0530

    db-endpoint

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "<DB_ENDPOINT>",
+    host: "database-1.cxjn11x2xgm7.ap-southeast-1.rds.amazonaws.com",
     user: "admin",
-    password: "<DB_PASSWORD>"
+    password: "qwerty12345"
 });
 
 con.connect(function(err) {
