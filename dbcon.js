bc60b716b7f88ee55fbb12f024993b0c6f45fb48
Pratik <kumar.pratik3@outlook.com>
2021-12-26 10:41:49 +0530

    create table

--- dbcon.js
+++ dbcon.js
@@ -6,8 +6,19 @@ const con = mysql.createConnection({
     password: "qwerty12345"
 });
 
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.end();
+// });
+
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+
+    con.query('CREATE DATABASE IF NOT EXISTS main;');
+    con.query('USE main;');
+    con.query('CREATE TABLE IF NOT EXISTS users(id int NOT NULL AUTO_INCREMENT, username varchar(30), email varchar(255), age int, PRIMARY KEY(id));', function(error, result, fields) {
+        console.log(result);
+    });
     con.end();
 });
