cc9ac7a7163b53a114b49e988c05594e8b8e70af
Pratik <kumar.pratik3@outlook.com>
2022-02-07 19:27:54 +0530

    nothing

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 907221691199.dkr.ecr.ap-southeast-1.amazonaws.com
-docke run -it -d -p 80:8080 907221691199.dkr.ecr.ap-southeast-1.amazonaws.com/docker_deploy:latest
+docker run -it -d -p 80:8080 907221691199.dkr.ecr.ap-southeast-1.amazonaws.com/docker_deploy:latest
